d9b1af9f62764c29a3158b8b853b1ce759652dcc
Lozan Vladlen <vnlozan@gmail.com>
2018-03-04 20:53:58 +0200

    manajer.js questions

--- scripts/manager.js
+++ scripts/manager.js
@@ -1,9 +1,16 @@
 var questionCounter = 0; 
 var userChoices = []; 
+var questionDiv = document.getElementById('questions_div');
+
+
+// var innerDiv = document.createElement('div');
+// innerDiv.className = 'block-2';
+// questionDiv.appendChild(innerDiv);
+
 readTextFile('data_q.json', function(allText) {
     var questions = JSON.parse(allText);
     console.log(questions);
-    
+	displayNext(questions);
 });
 function readTextFile(file, callback){
     var rawFile = new XMLHttpRequest();
@@ -20,4 +27,19 @@ function readTextFile(file, callback){
         }
     }
     rawFile.send(null);
+}
+function displayNext(questions){
+	// console.log(questions.length);
+	if(questionCounter<questions.length){
+        clearQuestionsDiv();
+        var label = document.createElement('label');
+        label.className = 'current_question';
+        label.innerText = questions[questionCounter].question;
+        questionDiv.appendChild(label);
+	}
+}
+function clearQuestionsDiv(){
+    while(questionDiv.firstChild){
+        questionDiv.removeChild(questionDiv.firstChild);
+    }
 }
