71d3c2ac653a12912b59ea8d857406026d8d8368
Lozan Vladlen <vnlozan@gmail.com>
2018-03-04 20:17:38 +0200

    project_update

--- scripts/manager.js
+++ scripts/manager.js
@@ -1,6 +1,22 @@
-(function() {
-	var questionCounter = 0; 
-	var userChoices = []; 
-	var quiz = $('#questions_div');
+var questionCounter = 0; 
+var userChoices = []; 
+readTextFile('data_q.json', function(allText) {
+    console.log(allText);
+});
 
-})();
+function readTextFile(file, callback){
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                callback(rawFile.responseText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
